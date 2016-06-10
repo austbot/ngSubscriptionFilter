961f119a641d143ee7f22e0861b79b27e4a1859f
Austin Adams <austin@fwdwebdesign.com>
2016-06-10 14:21:01 -0700

    Filter code implemented

--- asyncFilter.js
+++ asyncFilter.js
@@ -1,1 +1,37 @@
-module.exports = ;
+module.exports = ['$timeout', function ($timeout) {
+
+  let subscriptions = {};
+  let dataCache = {};
+
+  function async(input, name) {
+    if (!name) throw new Error('Async filter needs a name for the Observable subscription');
+    //Throw if not observavble
+    if (!('subscribe' in input)) throw new Error('Filter input is not and Observable');
+    //Setup subscription if we don't have one on file
+    if (!(name in subscriptions)) {
+      //Make sure input is an observable.
+      subscriptions[name] = input.subscribe(
+        //Subscribe to observable
+        function (data) {
+          $timeout(function () {
+            dataCache[name] = data;
+          });
+        },
+        //Subscribe to error
+        function (error) {
+          throw new Error(error);
+        },
+        //Subscribe to complete
+        function () {
+          //subscription complete TODO
+        });
+      //on complete then shut down subscription and if possible remove stateful flag
+    } else {
+      return dataCache[name];
+    }
+  }
+
+  async.$stateful = true;
+  return async;
+
+}];
--- index.js
+++ index.js
@@ -1,17 +1,5 @@
+import {asyncFilter} from './asyncFilter';
+
 angular.module('ngSubscription', [])
-  .filter('async', [function () {
-
-    function async(input) {
-      //Make sure input is an observable.
-
-      //Subscribe to observable.
-
-      //subscribe to error,
-
-      //on complete then shut down subscription and if possible remove stateful flag
-    }
-
-    async.$stateful = true;
-    return async;
-
-  }]);
+  
+  .filter('async', asyncFilter);
